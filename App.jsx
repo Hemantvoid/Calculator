7bf7cdf54b9f1473d8c861c428a9c2f6866c4bb4
Hemantvoid <sahu74110@gmail.com>
2025-04-23 22:44:10 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Calculater from './calculater'
+
+function App() {
+  var a=parseInt(document.getElementsByClassName("container").value)
+  
+  return (
+    <>
+    
+    <Calculater/>
+    </>
+  )
+}
+
+export default App
